399863e499fe21f8c0a1844da3bb3a09c1441348
Judy@LAPTOP <judylight2001@gmail.com>
2018-04-08 18:37:34 -0700

    lesson 4

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:launchcode@
 app.config['SQLALCHEMY-ECHO'] = True
 db = SQLAlchemy(app)
 
+
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
+        self.completed = False
 
 
 tasks = []
@@ -30,8 +33,9 @@ def index():
         db.session.add(new_task)
         db.session.commit()
     
-    tasks = Task.query.all()
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+    tasks = Task.query.filter_by(completed=False).all()
+    completed_tasks = Task.query.filter_by(completed=True).all()
+    return render_template('todos.html', title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
@@ -39,7 +43,8 @@ def delete_task():
 
     task_id = int(request.form['task-id'])
     task = Task.query.get(task_id)
-    db.session.delete(task)
+    task.completed = True
+    db.session.add(task)
     db.session.commit()
 
     return redirect('/')
