98887e179d2c9019d9099cb84a9edf65af216326
Judy@LAPTOP <judylight2001@gmail.com>
2018-04-10 14:55:51 -0700

    corrections

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:launchcode@localhost:8889/get-it-done'
-# Note:  The connection string after :// contains the following info:
-# user:password@server:portNumber/databaseName
-app.config['SQLALCHEMY-ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
@@ -21,9 +19,6 @@ class Task(db.Model):
         self.completed = False
 
 
-tasks = []
-
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
@@ -32,10 +27,11 @@ def index():
         new_task = Task(task_name)
         db.session.add(new_task)
         db.session.commit()
-    
+
     tasks = Task.query.filter_by(completed=False).all()
     completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html', title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
+    return render_template('todos.html',title="Get It Done!", 
+        tasks=tasks, completed_tasks=completed_tasks)
 
 
 @app.route('/delete-task', methods=['POST'])
@@ -50,5 +46,5 @@ def delete_task():
     return redirect('/')
 
 
-if __name__ =='__main__':
-    app.run()
+if __name__ == '__main__':
+app.run()
