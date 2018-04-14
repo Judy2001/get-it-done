104fe3c759f018d6026b489b5f25e24a05620efe
Judy@LAPTOP <judylight2001@gmail.com>
2018-04-14 12:13:26 -0700

    starting over

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-# Note:  The connection string after :// contains the following info:
-# user:password@server:portNumber/databaseName
-
-
-
-
-
 
 tasks = []
-
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -19,11 +11,8 @@ def index():
     if request.method == 'POST':
         task = request.form['task']
         tasks.append(task)
-    
-    return render_template('todos.html', title="Get It Done!", tasks=tasks)
+
+    return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
 
-
-
-
-    app.run()
+app.run()
